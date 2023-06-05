976d8081dd9d27f127f534c5ad7643ed17c9389f
PutriHabibahDevina <putrihdm13@gmail.com>
2023-06-05 10:42:01 +0700

    exercisedua_109

--- exercisedua_109/exercisedua_109.cpp
+++ exercisedua_109/exercisedua_109.cpp
@@ -1,2 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class pengarang {
+public:
+	string nama;
+	pengarang(string pNama) :
+		nama(pNama) {
+		cout << "Daftar pengarang pada penerbit " << nama << " :\n" << endl;
+	}
+};
+
+class penerbit {
+public:
+	string nama;
+	penerbit(string pNama) :
+		nama(pNama) {
+		cout << "\nDaftar penerbit yang diikuti " << nama << " :\n" << endl;
+	}
+};
+
+class buku {
+public:
+	string judul;
+	buku(string pNama) :
+		judul(pNama) {
+		cout << "\nDaftar buku yang dikarang " << judul << " :\n" << endl;
+	}
+
+};
+
+int main() {
+	pengarang;
+	penerbit;
+	buku;
+	return 0;
+}
